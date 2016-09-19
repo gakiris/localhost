c4f3f79d27f238646930e55251927759eb885cb3
gakiris <gakiris@hotmail.com>
2016-09-19 19:26:14 +0300

    Create BinaryGap.java

---
+++ BinaryGap.java
@@ -0,0 +1,29 @@
+int solution(int N){
+	
+	int N = 1041;
+	String gak = Integer.toBinaryString(N);
+      //System.out.println(gak);
+      int zeroCount = 0;
+      int maxZeroCount= 0;
+      for (int i = 0; i<gak.length(); i++){
+          
+          if (gak.charAt(i)=='0')
+          {
+              zeroCount++;
+          }
+          else
+          {
+              //System.out.println("");
+              if(maxZeroCount>zeroCount)
+              {
+                  zeroCount = 0;
+              }
+              else{ 
+                  maxZeroCount = zeroCount;
+                  zeroCount = 0;
+              }
+          }
+            
+      }
+    return maxZeroCount;
+  }
